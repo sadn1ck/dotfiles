018568902e1f7a26fb2d6f5e247a61e872690171
Anik Das <anikdas0811@gmail.com>
2021-03-27 11:04:18 +0530

    init dotfiles
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env sh
+
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/picom.conf ~/.config/picom.conf
+ln -s ~/dotfiles/omb/themes/custom/custom.theme.sh ~/.oh-my-bash/custom/themes/custom/
+ln -s ~/dotfiles/i3 ~/.config/i3
+ln -s ~/dotfiles/kitty ~/.config/kitty
+ln -s ~/dotfiles/polybar ~/.config/polybar
