c5125474d8070d37a9c0360291f0660a38ce0d0d
Anik Das <anikdas0811@gmail.com>
2022-02-27 10:59:18 +0530

    feat: new
    Signed-off-by: Anik Das <anikdas0811@gmail.com>

--- polybar/scripts/toggleredshift.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-result=$(ps -C redshift | grep -o redshift)
-if [[ $result == "redshift" ]]
-then
-redshift -x && killall redshift && notify-send "Screen Temperature Reset" -t 2000 
-else
-redshift -l 43.4:79.2  &
-notify-send "Redshift on" -t 2000
-fi
